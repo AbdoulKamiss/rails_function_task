1520c0136985af23e130729cff293c836945f41d
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-05-14 15:16:24 +0000

    commit

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,4 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-    def welcome
-        UserMailer.with(to: "test@example.com", name: "test").welcome
-    end
+    UserMailer.with(to: "test@example.com", name: "test").welcome
 end
